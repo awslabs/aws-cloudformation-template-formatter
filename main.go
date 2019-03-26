100d28ed0a8512b871c051b18462933ce43118a6
Steve Engledow <steve@offend.me.uk>
2019-03-26 14:05:42 +0000

    Add support for reading from stdin

--- main.go
+++ main.go
@@ -7,64 +7,73 @@ import (
 
 	"github.com/awslabs/aws-cloudformation-template-formatter/format"
 	"github.com/awslabs/aws-cloudformation-template-formatter/parse"
+
+	"github.com/andrew-d/go-termutil"
+	"github.com/spf13/pflag"
 )
 
-var usage = `Usage: cfn-format [-j] <filename>
+var usage = `Usage: cfn-format [OPTION...] [FILENAME]
 
   AWS CloudFormation Format is a tool that reads a CloudFormation template
   and outputs the same template, formatted according to the same standards
   used in AWS documentation.
 
+  If FILENAME is not supplied, cfn-format will read from STDIN.
+
 Options:
-  -j        Output the template as JSON (default format: YAML).
-  -w        Write the output back to the file rather than to stdout.
-  --help    Show this message and exit.
-`
+  --help    Show this message and exit.`
+
+var jsonFlag bool
+var writeFlag bool
+
+func init() {
+	pflag.BoolVarP(&jsonFlag, "json", "j", false, "Output the template as JSON (default format: YAML).")
+	pflag.BoolVarP(&writeFlag, "write", "w", false, "Write the output back to the file rather than to stdout.")
+
+	pflag.Usage = func() {
+		fmt.Fprintln(os.Stderr, usage)
+		pflag.PrintDefaults()
+		os.Exit(1)
+	}
+}
 
 func die(message string) {
 	fmt.Fprintln(os.Stderr, message)
 	os.Exit(1)
 }
 
-func help() {
-	die(usage)
-}
-
 func main() {
-	style := "yaml"
-	write := false
+	var fileName string
+	var source map[string]interface{}
+	var err error
 
-	// Parse options
-	if len(os.Args) < 2 {
-		help()
-	}
+	pflag.Parse()
+	args := pflag.Args()
 
-	for _, arg := range os.Args[1 : len(os.Args)-1] {
-		switch arg {
-		case "-j":
-			style = "json"
-		case "-w":
-			write = true
-		case "-h", "--help":
-			help()
+	if len(args) == 1 {
+		// Reading from a file
+		fileName = args[0]
+		source, err = parse.ReadFile(fileName)
+		if err != nil {
+			die(err.Error())
 		}
-	}
+	} else if !termutil.Isatty(os.Stdin.Fd()) {
+		if writeFlag {
+			// Can't use write without a filename!
+			die("Can't write back to a file when reading from stdin")
+		}
 
-	// Get the filename
-	fileName := os.Args[len(os.Args)-1]
-	if fileName == "--help" {
-		help()
-	}
-
-	// Read the source template
-	source, err := parse.ReadFile(fileName)
-	if err != nil {
-		die(err.Error())
+		source, err = parse.Read(os.Stdin)
+		if err != nil {
+			die(err.Error())
+		}
+	} else {
+		pflag.Usage()
 	}
 
 	// Format the output
 	var output string
-	if style == "json" {
+	if jsonFlag {
 		output = format.Json(source)
 	} else {
 		output = format.Yaml(source)
@@ -76,7 +85,7 @@ func main() {
 		die(err.Error())
 	}
 
-	if write {
+	if writeFlag {
 		ioutil.WriteFile(fileName, []byte(output), 0644)
 	} else {
 		fmt.Println(output)
