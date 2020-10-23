b9c4826792594bfd6d516926dbdcbe48b4981cfc
Steve Engledow <sengledo@amazon.co.uk>
2020-10-23 16:58:50 +0100

    Improve build script

--- build.sh
+++ build.sh
@@ -1,33 +1,53 @@
 #!/bin/bash
 
-# This script will build cfn-format for all platforms
+# This script will build rain for all platforms
+
+set -e
+
+NAME=cfn-format
+OUTPUT_DIR=dist
+
+VERSION=$(git describe)
+
+declare -A PLATFORMS=([linux]=linux [darwin]=macos [windows]=windows)
+declare -A ARCHITECTURES=([386]=i386 [amd64]=amd64 [arm]=arm [arm64]=arm64)
 
 # Run tests first
-
+golint -set_exit_status ./... || exit 1
 go vet ./... || exit 1
-
 go test ./... || exit 1
 
-declare -A platforms=([linux]=linux [darwin]=osx [windows]=windows)
-declare -A architectures=([386]=i386 [amd64]=amd64)
+echo "Building $NAME $VERSION..."
 
-DESTDIR=dist
-
-mkdir -p $DESTDIR
-
-echo "Building cfn-format"
-
-for platform in ${!platforms[@]}; do
-    for architecture in ${!architectures[@]}; do
-        echo "... $platform $architecture..."
-
-        name=cfn-format-${platforms[$platform]}-${architectures[$architecture]}
-
-        if [ "$platform" == "windows" ]; then
-            name=${name}.exe
+for platform in ${!PLATFORMS[@]}; do
+    for architecture in ${!ARCHITECTURES[@]}; do
+        if [[ "$architecture" == arm* && "$platform" != "linux" ]]; then
+            continue
         fi
 
-        GOOS=$platform GOARCH=$architecture go build -o $DESTDIR/$name ./cmd/cfn-format/*
+        if [[ "$architecture" == "386" && "$platform" == "darwin" ]]; then
+            continue
+        fi
+
+        echo "$platform/$architecture..."
+
+        full_name="${NAME}-${VERSION}_${PLATFORMS[$platform]}-${ARCHITECTURES[$architecture]}"
+        bin_name="$NAME"
+
+        if [ "$platform" == "windows" ]; then
+            bin_name="${NAME}.exe"
+        fi
+
+        mkdir -p "$OUTPUT_DIR/$full_name"
+
+        eval GOOS=$platform GOARCH=$architecture go build -o "$OUTPUT_DIR/${full_name}/${bin_name}" "./cmd/cfn-format/*"
+        cp LICENSE "$OUTPUT_DIR/$full_name"
+        cp README.md "$OUTPUT_DIR/$full_name"
+
+        cd "$OUTPUT_DIR"
+        zip -9 -r "${full_name}.zip" "$full_name"
+        rm -r "$full_name"
+        cd -
     done
 done
 
